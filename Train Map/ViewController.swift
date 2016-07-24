f7e1690ca27749e3fd4a8c0e8e0da65309e26625
Tom Curtis <t@dinosaursandmoustaches.com>
2016-07-24 09:04:21 -0700

    Initial Commit

---
+++ Train Map/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Train Map
+//
+//  Created by Tom Curtis on 24 Jul 2016.
+//  Copyright © 2016 Tom Curtis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
